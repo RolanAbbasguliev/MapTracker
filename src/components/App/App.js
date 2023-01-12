1d6cdb177563729143b82aa3836e83924fc17902
RolanAbbasguliev <abbarolan@gmail.com>
2023-01-12 15:03:24 +0300

    Init with leaflet map and 'Antd' coords table

---
+++ src/components/App/App.js
@@ -0,0 +1,140 @@
+import React, { useState } from 'react';
+import "./App.css";
+import { Table } from 'antd';
+
+import L from 'leaflet';
+import { MapContainer, TileLayer, useMap, Marker, Popup } from 'react-leaflet'
+import 'leaflet/dist/leaflet.css';
+
+import icon from 'leaflet/dist/images/marker-icon.png';
+import iconShadow from 'leaflet/dist/images/marker-shadow.png';
+
+let DefaultIcon = L.icon({
+    iconUrl: icon,
+    shadowUrl: iconShadow
+});
+
+L.Marker.prototype.options.icon = DefaultIcon;
+
+
+const dataSource = [
+    {
+        key: '1',
+        num: '№1',
+        StartLat: 59.84660399,
+        StartLng: 30.29496392,
+        EndLat: 59.82934196,
+        EndLng: 30.42423701
+    },
+    {
+        key: '2',
+        num: '№2',
+        StartLat: 59.82934196,
+        StartLng: 30.42423701,
+        EndLat: 59.82761295,
+        EndLng: 30.41705607
+    },
+    {
+        key: '3',
+        num: '№3',
+        StartLat: 59.83567701,
+        StartLng: 30.38064206,
+        EndLat: 59.84660399,
+        EndLng: 30.29496392
+    },
+    {
+        key: '4',
+        num: '№4',
+        StartLat: 59.84660399,
+        StartLng: 30.29496392,
+        EndLat: 59.82761295,
+        EndLng: 30.41705607
+    },
+    {
+        key: '5',
+        num: '№5',
+        StartLat: 59.83567701,
+        StartLng: 30.38064206,
+        EndLat: 59.84660399,
+        EndLng: 30.29496392
+    },
+
+
+];
+
+const columns = [
+    {
+        title: 'Номер Заявки',
+        dataIndex: 'num',
+        key: 'num',
+    },
+    {
+        title: 'Координаты ОТ lat',
+        dataIndex: 'StartLat',
+        key: 'StartLat',
+    },
+    {
+        title: 'Координаты ОТ lng',
+        dataIndex: 'StartLng',
+        key: 'StartLng',
+    },
+    {
+        title: 'Координаты ОТ lat',
+        dataIndex: 'EndLat',
+        key: 'EndLat',
+    },
+    {
+        title: 'Координаты ОТ lng',
+        dataIndex: 'EndLng',
+        key: 'EndLng',
+    },
+];
+const App = () => {
+    const [selectedRow, setSelectedRow] = useState({});
+
+
+    return (
+        <div className="main_wrapper">
+            <Table
+                className="Table"
+                dataSource={dataSource}
+                columns={columns}
+                rowSelection={false}
+                pagination={false}
+                onRow={(record) => ({
+                    onClick: () => {
+                        setSelectedRow(record);
+                        console.log(record);
+                    }
+                })}
+            />
+
+            <MapContainer center={[59.84660399, 30.29496392]} zoom={12} scrollWheelZoom={true}>
+                <TileLayer
+                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+                {Object.keys(selectedRow).length !== 0 &&
+                    <React.Fragment>
+                        <Marker position={[selectedRow.StartLat, selectedRow.StartLng]}>
+                            <Popup>
+                                A pretty CSS3 popup. <br /> Easily customizable.
+                            </Popup>
+                        </Marker>
+
+                        <Marker position={[selectedRow.EndLat, selectedRow.EndLng]}>
+                            <Popup>
+                                A pretty CSS3 popup. <br /> Easily customizable.
+                            </Popup>
+                        </Marker>
+                    </React.Fragment>
+
+                }
+
+            </MapContainer>
+
+        </div >
+    );
+};
+
+export default App;
